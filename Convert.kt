3754497aad812fd747eaae81e967165fcf154418
Deepshika Ghale <58839663+DeepshikaGhale@users.noreply.github.com>
2023-02-01 12:42:22 -0500

    Initialize variables to collect data

--- Convert.kt
+++ Convert.kt
@@ -1,3 +1,13 @@
 fun main(){
+    print("Please enter a number:")
 
+    var input = readLine()
+    var inputNumber = input!!.toFloat()
+
+
+    print("Enter unit of measurement:")
+    var unitOfMeasurement = readLine()
+
+    var inputData = "$inputNumber$unitOfMeasurement"
+    print(inputData)
 }
