97317d3f2070c897e60f643b0153bd2806ae2a5f
Deepshika Ghale <58839663+DeepshikaGhale@users.noreply.github.com>
2023-02-02 11:33:02 -0500

    Create method to validate if the measurement is available or not

--- Convert.kt
+++ Convert.kt
@@ -1,44 +1,68 @@
 fun main(){
-
-    var inputNumber = getInputNumber()
+    //collect valid input number from getInputNumber function
+    val inputNumber = getInputNumber()
 
     print("Enter unit of measurement:")
-    var unitOfMeasurement = readLine()
+    //collect unit of measurement for conversion
+    val unitOfMeasurement = readLine()
 
+    //collect the final result after validating the input data
+    val result: String = validateMeasurementUnit(inputNumber, unitOfMeasurement!!)
 
-    var result: String = convertMeasurementUnit(inputNumber, unitOfMeasurement!!)
-
+    //provide output
     println(result)
+}
+
+//validate measurement unit
+fun validateMeasurementUnit(inputNumber: Float, unitOfMeasurement: String): String{
+    //store the result from convertMeasurementUnit function
+    var result: String = convertMeasurementUnit(inputNumber, unitOfMeasurement)
+
+    /*
+     in case the measurement unit is not found then while loop
+     runs to ask user to enter the measurement unit again
+    */
+    while (result == "not found"){
+
+        println("The provided measurement unit is not found.")
+        print("Enter unit of measurement:")
+        //collect the measurement unit
+        val validMeasurementUnit = readLine()
+        result =  convertMeasurementUnit(inputNumber, validMeasurementUnit!!)
+    }
+
+    return result
 }
 
 //logic for conversion
 fun convertMeasurementUnit(inputNumber: Float, unitOfMeasurement: String): String{
 
     //collect conversion number and unit
-    var inputData: String = "$inputNumber$unitOfMeasurement"
+    val inputData= "$inputNumber$unitOfMeasurement"
 
     //collect result
-    var result: String
+    val conversionResult: String
 
     /*
      use of when statement for conversion, similar to switch, when any condition satisfies the entered
      unit of measurement then it converts it to respective unit otherwise it enters the else statement
     */
+
     when (unitOfMeasurement){
-        "km" -> result = "$inputData =  ${inputNumber * 0.62}mi"
-        "mi" -> result = "$inputData = ${inputNumber * 1.61}km"
-        "cm" -> result = "$inputData = ${inputNumber * 0.39}in"
-        "in" -> result = "$inputData = ${inputNumber * 2.54}cm"
-        "kg" -> result = "$inputData = ${inputNumber * 2.2}lb"
-        "lb" -> result = "$inputData =  ${inputNumber * 0.45}kg"
-        "g" -> result = "$inputData =  ${inputNumber * 0.04}oz"
-        "oz" -> result = "$inputData =  ${inputNumber * 28.35}g"
-        "C" -> result = "$inputData =  ${(inputNumber * (9/5))+32}F"
-        "L" -> result = "$inputData =  ${inputNumber * 4.17}cups"
-        "cup" -> result = "$inputData =  ${inputNumber * 0.24}L"
-        else -> result = "$unitOfMeasurement cannot be converted."
+        "km" -> conversionResult = "$inputData =  ${inputNumber * 0.62}mi"
+        "mi" -> conversionResult = "$inputData = ${inputNumber * 1.61}km"
+        "cm" -> conversionResult = "$inputData = ${inputNumber * 0.39}in"
+        "in" -> conversionResult = "$inputData = ${inputNumber * 2.54}cm"
+        "kg" -> conversionResult = "$inputData = ${inputNumber * 2.2}lb"
+        "lb" -> conversionResult = "$inputData =  ${inputNumber * 0.45}kg"
+        "g" -> conversionResult = "$inputData =  ${inputNumber * 0.04}oz"
+        "oz" -> conversionResult = "$inputData =  ${inputNumber * 28.35}g"
+        "C" -> conversionResult = "$inputData =  ${(inputNumber * (9.0/5))+32}F"
+        "L" -> conversionResult = "$inputData =  ${inputNumber * 4.17}cups"
+        "cup" -> conversionResult = "$inputData =  ${inputNumber * 0.24}L"
+        else -> conversionResult = "not found" //return not found if the measurement unit is not available
     }
-    return result
+    return conversionResult
 }
 
 
@@ -47,8 +71,8 @@ fun getInputNumber(): Float {
     //collect the input number
     var input = 0f
 
-    //validate if the input if number or not
-    var isInputValid: Boolean = false
+    //validate if the input number or not
+    var isInputValid = false
 
     //loop runs until the input is valid
     while (!isInputValid) {
