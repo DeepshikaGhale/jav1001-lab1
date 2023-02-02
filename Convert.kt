59ee16f910ace6cf661b28482ab203e71a305123
Deepshika Ghale <58839663+DeepshikaGhale@users.noreply.github.com>
2023-02-02 11:07:07 -0500

    Create separate method for conversion logic

--- Convert.kt
+++ Convert.kt
@@ -5,10 +5,25 @@ fun main(){
     print("Enter unit of measurement:")
     var unitOfMeasurement = readLine()
 
-    var inputData = "$inputNumber$unitOfMeasurement"
 
+    var result: String = convertMeasurementUnit(inputNumber, unitOfMeasurement!!)
+
+    println(result)
+}
+
+//logic for conversion
+fun convertMeasurementUnit(inputNumber: Float, unitOfMeasurement: String): String{
+
+    //collect conversion number and unit
+    var inputData: String = "$inputNumber$unitOfMeasurement"
+
+    //collect result
     var result: String
 
+    /*
+     use of when statement for conversion, similar to switch, when any condition satisfies the entered
+     unit of measurement then it converts it to respective unit otherwise it enters the else statement
+    */
     when (unitOfMeasurement){
         "km" -> result = "$inputData =  ${inputNumber * 0.62}mi"
         "mi" -> result = "$inputData = ${inputNumber * 1.61}km"
@@ -18,14 +33,12 @@ fun main(){
         "lb" -> result = "$inputData =  ${inputNumber * 0.45}kg"
         "g" -> result = "$inputData =  ${inputNumber * 0.04}oz"
         "oz" -> result = "$inputData =  ${inputNumber * 28.35}g"
-
         "C" -> result = "$inputData =  ${(inputNumber * (9/5))+32}F"
         "L" -> result = "$inputData =  ${inputNumber * 4.17}cups"
         "cup" -> result = "$inputData =  ${inputNumber * 0.24}L"
         else -> result = "$unitOfMeasurement cannot be converted."
     }
-
-    println(result)
+    return result
 }
 
 
